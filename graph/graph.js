c90a6b62c9472b7ae283cbf35a7c9cc8a64a36cb
Jacob Tepperman <jtepp@icloud.com>
2021-07-31 17:33:31 -0400

    finished graph

--- graph/graph.js
+++ graph/graph.js
@@ -1,71 +1,54 @@
 class Result {
-    constructor(obj) {
-        this.id = obj.id;
-        this.lower = obj.lower;
-        this.higher = obj.higher;
+    // constructor(obj) {
+    //     this.id = obj.id;
+    //     this.lower = obj.lower;
+    //     this.higher = obj.higher;
+    // }
+    constructor() {
+        this.id = allResults.length
+        this.lower = Math.round(1220 + Math.random() * 200)
+        this.higher = Math.round(this.lower + Math.random() * 150 + 50)
     }
 }
 
 const grph = document.getElementById("gr1")
 
 let allResults = [];
+let avg;
 
 (async function main() {
 
-    await fetch("https://raw.githubusercontent.com/jtepp/Alexander-Academy/main/graph/data.json").then(res => res.json()).then(json => {
+    // await fetch("https://raw.githubusercontent.com/jtepp/Alexander-Academy/main/graph/data.json").then(res => res.json()).then(json => {
 
-        json.forEach(result => {
-            allResults.push(new Result(result))
-        })
+    //     json.forEach(result => {
+    //         allResults.push(new Result(result))
+    //     })
+    // })
+
+    const resultCount = 30;
+
+    for (let i = 0; i < resultCount; i++) {
+        allResults.push(new Result())
+    }
+    allResults.sort(function (a, b) {
+        return a.higher - b.higher
     })
-
-    const avg = new Result({ //made average object, id is the next index, so calling it will now be end index (-1)
+    avg = new Result({ //made average object, id is the next index, so calling it will now be end index (-1)
         id: `${allResults.length}`,
         lower: average(allResults.map(result => result.lower)),
         higher: average(allResults.map(result => result.higher))
     })
-    allResults.push(avg)
+    // allResults.push(avg) result.id == `${allResults.length-1}`
     allResults.forEach(result => {
-        document.getElementById("gr1-data").appendChild(returnDataElement(result, result.id == `${allResults.length-1}`))
+        document.getElementById("gr1-data").appendChild(returnDataElement(result, false))
     })
+    document.getElementById("gr1-data").appendChild(returnDataElement(avg, true))
 
 })()
 
-// grph.onclick = function (e) {
-//     const base = grph.getAttribute("base")
-//     const top = grph.getAttribute("top")
-//     const scale = (top - base) / graphSize("gr1", "height")
-
-//     if (!this.classList.contains("opened")) { //if not opened, open it
-//         for (let p of document.getElementsByClassName("point-range")) {
-//             const id = p.getAttribute("id")
-//             console.log(id)
-//             p.style.height = `${(allResults[id].higher - allResults[id].lower) * scale}px`
-//         }
-//         grph.classList.add("opened")
-//     } else { //if opened, close it
-//         for (let p of document.getElementsByClassName("point-range")) {
-//             p.style.height = `0px`
-//         }
-//         grph.classList.remove("opened")
-//     }
-// }
-
-for (let b of document.getElementsByClassName("test-button")) {
-
-    b.onclick = function (e) {
-        if (b.id == "reset") {
-            closeGraph("gr1")
-        } else {
-            document.documentElement.style = "--graph-transition: " + e.target.getAttribute("transition") + ";"
-
-            openGraph("gr1")
-
-
-        }
-    }
+grph.onclick = function (e) {
+    openGraph(grph.id)
 }
-
 
 function returnDataElement(result, isAvg) {
     const base = grph.getAttribute("base")
@@ -73,33 +56,42 @@ function returnDataElement(result, isAvg) {
 
     const scale = (top - base) / graphSize("gr1", "height")
 
-    const cont = document.createElement("div")
-    cont.className = "point-cont" + (isAvg ? " avg" : "")
-    cont.style.width = graphSize("gr1", "width") / allResults.length + "px"
+    if (isAvg) {
+        const avgcont = document.createElement("div")
+        avgcont.id = "average-cont"
+        avgcont.style.bottom = `${(result.lower - base) * scale}px`
+        // avgcont.style.height = `${(result.higher - result.lower) * scale}px`
+        avgcont.setAttribute("value", (result.higher - result.lower))
+        return avgcont
+    } else {
+        const cont = document.createElement("div")
+        cont.className = "point-cont" // + (isAvg ? " avg" : "")
+        cont.style.width = graphSize("gr1", "width") / (allResults.length - 1) + "px"
 
-    const range = document.createElement("div")
-    range.classList.add("point-range")
-    range.setAttribute("id", result.id)
-    range.setAttribute("improvement", `+${result.higher - result.lower}`)
-    range.style.bottom = `${(result.lower - base) * scale}px`
+        const range = document.createElement("div")
+        range.classList.add("point-range")
+        range.setAttribute("id", result.id)
+        range.setAttribute("improvement", `+${result.higher - result.lower}`)
+        range.style.bottom = `${(result.lower - base) * scale}px`
 
 
-    let lower = document.createElement("div")
-    lower.classList.add("point")
-    lower.classList.add("lower")
-    lower.setAttribute("value", result.lower)
+        let lower = document.createElement("div")
+        lower.classList.add("point")
+        lower.classList.add("lower")
+        lower.setAttribute("value", result.lower)
 
-    let higher = document.createElement("div")
-    higher.classList.add("point")
-    higher.classList.add("higher")
-    higher.setAttribute("value", result.higher)
+        let higher = document.createElement("div")
+        higher.classList.add("point")
+        higher.classList.add("higher")
+        higher.setAttribute("value", result.higher)
 
-    range.appendChild(lower)
-    range.appendChild(higher)
+        range.appendChild(lower)
+        range.appendChild(higher)
 
-    cont.appendChild(range)
+        cont.appendChild(range)
 
-    return cont
+        return cont
+    }
 }
 
 function openGraph(gId) {
@@ -114,11 +106,12 @@ function openGraph(gId) {
             console.log(id)
             p.style.height = `${(allResults[id].higher - allResults[id].lower) * scale}px`
         }
+        document.getElementById("average-cont").style.height = `${(avg.higher - avg.lower) * scale}px`
         cGraph.classList.add("opened")
         //     }
     } else {
         closeGraph(gId)
-        setTimeout(() => openGraph(gId), 1000)
+        // setTimeout(() => openGraph(gId), 1000)
     }
 }
 
@@ -126,6 +119,7 @@ function closeGraph(gId) {
     for (let p of document.querySelectorAll("#" + gId + " .point-range")) {
         p.style.height = `0px`
     }
+    document.getElementById("average-cont").style.height = `0px`
     document.getElementById(gId).classList.remove("opened")
 }
 
